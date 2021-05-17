82597c4740f4a2a1d6c8aaa0844acf25be6b83ba
melissa-koi <melissawangui3@gmail.com>
2021-05-17 18:26:47 +0300

    able photo.html and able to access it using image id

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('photo/<str:pk>/', views.viewPhoto, name = 'photo'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -3,17 +3,17 @@ from .models import Location, Category, Image
 # Create your views here.
 
 def home(request):
-
     category = request.GET.get('category')
     if category is None:
         gallery = Image.objects.all()
     else:
         gallery = Image.objects.filter(category__name=category)
 
-    
     location = Location.objects.all()
     categories = Category.objects.all()
 
-
-
     return render(request, 'home.html', {"galleries": gallery, "location": location, "categories": categories})
+
+def viewPhoto(request, pk):
+    photo =Image.objects.get(id=pk)
+    return render(request, 'photo.html', {"photo": photo})
