5fe9d02934c6bc4de554ec2f1ea90edd9630e853
melissa-koi <melissawangui3@gmail.com>
2021-05-17 21:03:02 +0300

    add navbar.html

--- gallery/views.py
+++ gallery/views.py
@@ -3,16 +3,16 @@ from .models import Location, Category, Image
 # Create your views here.
 
 def home(request):
-    category = request.GET.get('category')
-    if category is None:
+    location = request.GET.get('location')
+    if location is None:
         gallery = Image.objects.all()
     else:
-        gallery = Image.objects.filter(category__name=category)
+        gallery = Image.objects.filter(location__name=location)
 
     location = Location.objects.all()
     categories = Category.objects.all()
 
-    return render(request, 'home.html', {"galleries": gallery, "location": location, "categories": categories})
+    return render(request, 'home.html', {"galleries": gallery, "locations": location, "categories": categories})
 
 def viewPhoto(request, pk):
     photo =Image.objects.get(id=pk)
