9d4b8ac797808205afd701332b66f5e549e51936
melissa-koi <melissawangui3@gmail.com>
2021-05-18 10:31:14 +0300

    fix spacing

--- Pgallery/settings.py
+++ Pgallery/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-9u^z&!#p&1y4h)#njr#j6#qaq7b3ggwb-n6ei=*nwf1e#=#1@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
